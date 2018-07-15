a0e17b9216b5c3bad60958e56fc8ca6374690685
ChomboRumi <30567363+ChomboRumi@users.noreply.github.com>
2018-07-15 21:22:47 +0200

    Add files via upload

--- charge.py
+++ charge.py
@@ -3,45 +3,16 @@
 import json
 import csv
 import re
+import pickle
 
+import nltk
+from nltk.tag import hmm
+from nltk.tag import tnt
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-def rellenarLabel(dataset,label):
-    dataset.append(label)
-    return dataset
-
-def generateBoW(frases,numeroPalabras):
-
-    for frase in frases:
-        string=normalize(frase)
-        bol=re.sub("[^a-z]"," ",string).split(" ")
-        for word in bol:
-            if len(word) <1:
-                continue
-            elif numeroPalabras.get(word) == None:
-                numeroPalabras[word]=1
-            else:
-                numeroPalabras[word] = numeroPalabras[word]+1
-    return numeroPalabras
-def reduceBow(numeroPalabras,n):
-    result={}
-    dict2={}
-    count=0
-    for i in numeroPalabras.keys():
-        if dict2.get(numeroPalabras[i]) == None:
-                dict2[numeroPalabras[i]]=[]
-                dict2[numeroPalabras[i]].append(i)
-        else:
-                dict2[numeroPalabras[i]].append(i)
-    for i in sorted(dict2.keys(), reverse=True):
-        if count > n :
-                break
-        for x in dict2[i]:
-                result[x]=i
-                count = count+1
-                if count > n :
-                      break
-    print(len(result))
-    return result
+import math
 
 def normalize(frase):
     string = frase.lower()
@@ -53,32 +24,47 @@ def normalize(frase):
     string = re.sub(u"[ýÿ]", 'y', string)
     return string
 
-def vectoritacio(frases,vocabulari):
-    fra=[]
+
+def rellenarLabel(dataset,label):
+    dataset.append(label)
+    return dataset
+
+def generateBoW(frases,numeroPalabras,bw):
+
     for frase in frases:
         string=normalize(frase)
-        bol= string.split(" ")
-        bolLocal={}
-        vector=[]
+        bol=re.sub("[^a-z]"," ",string).split(" ")
         for word in bol:
-            if bolLocal.get(word) == None:
-                bolLocal[word]=1
-            else:
-                bolLocal[word] = bolLocal[word]+1
-        for word in vocabulari.keys():
-            if bolLocal.get(word) != None:
-                vector.append(str(bolLocal[word]/vocabulari[word]))
-            else:
-                vector.append(str(0))
-        fra.append(vector)
-
-    return fra
-
-
+                if numeroPalabras.get(word) == None:
+                      numeroPalabras[word]=1
+                else:
+                      numeroPalabras[word] = numeroPalabras[word]+1
+    return numeroPalabras
+def reduceBow(numeroPalabras,n):
+    result={}
+    dict2={}
+    count=0
+    for i in numeroPalabras.keys():
+        if dict2.get(numeroPalabras[i]) == None:
+                dict2[numeroPalabras[i]]=[]
+                dict2[numeroPalabras[i]].append(i)
+        else:
+                dict2[numeroPalabras[i]].append(i)
+    for i in sorted(dict2.keys()):
+        if count > n :
+                break
+        for x in dict2[i]:
+                if count < 10:
+                    print(x)
+                result[x]=i
+                count = count+1
+                if count > n :
+                      break
+    return dict2
 
 dicId = {}
 bolsaPalabras={}
-with open("truthTest.txt", 'rt') as csvfile:
+with open("/home/ramon/Escritorio/text/pan-ap17-bigdata/test/truth.txt", 'rt') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=':')
     for row in spamreader:
         dic = {}
@@ -87,7 +73,7 @@ with open("truthTest.txt", 'rt') as csvfile:
             dic["nacionalidad"]=row[6]
             dicId[row[0]] = dic
 dicIdTrain = {}
-with open("truth.txt", 'rt') as csvfile:
+with open("/home/ramon/Escritorio/text/pan-ap17-bigdata/training/truth.txt", 'rt') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=':')
     for row in spamreader:
         dic = {}
@@ -96,27 +82,24 @@ with open("truth.txt", 'rt') as csvfile:
             dic["nacionalidad"]=row[6]
             dicIdTrain[row[0]] = dic
 
-
-
-
-
-
-
 vocabularioNacional = {}
 vocabularioNacionalTest = {}
-
-with open("frases_training.json") as json_data:
+fileObject = open("tntTrained",'rb')
+b = pickle.load(fileObject)
+with open("/home/ramon/Escritorio/text/frases_training.json") as json_data:
     data = json.load(json_data)
     lexicon={}
     for id in data.keys():
-        lexicon=generateBoW(data[id],lexicon)
+        lexicon=generateBoW(data[id],lexicon,b)
     bolsaPalabras=reduceBow(lexicon,500)
 
-with open('bagOfWords.json', 'w') as outfile:
-    json.dump(bolsaPalabras, outfile)
 
+dicRepeticionesNumeroDePalabras={}
+for i in sorted(bolsaPalabras.keys()):
+      dicRepeticionesNumeroDePalabras[math.log( i )]=math.log(len(bolsaPalabras[i]))
+pdf = pd.DataFrame(list(dicRepeticionesNumeroDePalabras.items()), columns=["repeticiones","numeroP"])
+print(dicRepeticionesNumeroDePalabras)
 
-
-
-
+plt.plot(pdf['repeticiones'],pdf['numeroP'])
+plt.show() # Depending on whether you use IPython or interactive mode, etc.
 
